69f5364d3dbf06019dbb37b838ee8e809fe36a77
SachinGadge13 <gadges14@gmail.com>
2022-09-04 18:46:35 +0530

    Added: Welcome Message

---
+++ src/com/bridgelabz/LineComparisonProblem/LineComparison.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.LineComparisonProblem;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program.");
+	}
+
+}
